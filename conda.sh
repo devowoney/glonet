b905d73ca54f5390a475366d60de668b3748fd71
devowoney <devowoney@gmail.com>
2025-07-06 15:17:49 +0000

    Fix a conda.sh

--- conda.sh
+++ conda.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-# Install minconda
-echo | curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh
+if [ ! -e "/home/onyxia/miniforge3/etc/profile.d/conda.sh" ]; then
 
-source miniconda3/etc/profile.d/conda.sh
+    # Install minconda
+    echo | curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+    bash Miniforge3-$(uname)-$(uname -m).sh
 
-# Install pakages for glonet from env_yaml file 
-conda update -n base -c conda-forge conda
-conda env create -n glon --file /glonet_daily_forecast_data_orchestration/conda_environment.yml
-conda init
 
-# Creat jupyter_notebook environment from conda environment
-# If you use jupyter service, activate (uncomment) the command line below :
-# ipython kernel install --user --name=glon
+    exec bash
 
-# ADD Cuda variables
-echo "export CUDA_HOME=/usr/local/cuda" >> ../.bashrc
-echo "export PATH=$CUDA_HOME/bin:$PATH" >> ../.bashrc
-echo "export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH" >> ../.bashrc
+else
+    source ~/miniforge3/etc/profile.d/conda.sh
+    
+    # Install pakages for glonet from env_yaml file 
+    conda update -n base -c conda-forge conda
+    conda env create -n glon --file ./glonet_daily_forecast_data_orchestration/conda_environment.yml
+    conda init
 
-# ADD user alias
-echo "alias c0='clear'" >> ../.bashrc
-echo "alias ..='cd ../'" >> ../.bashrc
-echo "alias ...='cd ../../'" >> ../.bashrc
-echo "alias lr='ls -alrth'" >> ../.bashrc
-echo "alias lr='ls -alRth'" >> ../.bashrc
-echo "alias r='more'" >> ../.bashrc
+    # Creat jupyter_notebook environment from conda environment
+    # If you use jupyter service, activate (uncomment) the command line below :
+    # ipython kernel install --user --name=glon
 
-# Restart bash
-source ../.bashrc
+    # ADD Cuda variables
+    echo "export CUDA_HOME=/usr/local/cuda" >> ../.bashrc
+    echo "export PATH=$CUDA_HOME/bin:$PATH" >> ../.bashrc
+    echo "export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH" >> ../.bashrc
+
+    # ADD user alias
+    echo "alias c0='clear'" >> ../.bashrc
+    echo "alias ..='cd ../'" >> ../.bashrc
+    echo "alias ...='cd ../../'" >> ../.bashrc
+    echo "alias lr='ls -alrth'" >> ../.bashrc
+    echo "alias lR='ls -alRth'" >> ../.bashrc
+    echo "alias r='more'" >> ../.bashrc
+
+    # Restart bash
+    source ../.bashrc
+    
+fi
