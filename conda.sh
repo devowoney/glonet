05620b675b799f466012e5ae54adbe88f07f3366
devowoney <devowoney@gmail.com>
2025-07-07 08:39:43 +0000

    Fix conda.sh and env.yaml

--- conda.sh
+++ conda.sh
@@ -6,8 +6,10 @@ if [ ! -e "/home/onyxia/miniforge3/etc/profile.d/conda.sh" ]; then
     echo | curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     bash Miniforge3-$(uname)-$(uname -m).sh
 
-
     exec bash
+
+    rm -rf Miniforge3-$(uname)-$(uname -m).sh
+    bash conda.sh
 
 else
     source ~/miniforge3/etc/profile.d/conda.sh
@@ -22,19 +24,19 @@ else
     # ipython kernel install --user --name=glon
 
     # ADD Cuda variables
-    echo "export CUDA_HOME=/usr/local/cuda" >> ../.bashrc
-    echo "export PATH=$CUDA_HOME/bin:$PATH" >> ../.bashrc
-    echo "export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH" >> ../.bashrc
+    echo "export CUDA_HOME=/usr/local/cuda" >> ~/.bashrc
+    echo "export PATH=$CUDA_HOME/bin:$PATH" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
 
     # ADD user alias
-    echo "alias c0='clear'" >> ../.bashrc
-    echo "alias ..='cd ../'" >> ../.bashrc
-    echo "alias ...='cd ../../'" >> ../.bashrc
-    echo "alias lr='ls -alrth'" >> ../.bashrc
-    echo "alias lR='ls -alRth'" >> ../.bashrc
-    echo "alias r='more'" >> ../.bashrc
+    echo "alias c0='clear'" >> ~/.bashrc
+    echo "alias ..='cd ../'" >> ~/.bashrc
+    echo "alias ...='cd ../../'" >> ~/.bashrc
+    echo "alias lr='ls -alrth'" >> ~/.bashrc
+    echo "alias lR='ls -alRth'" >> ~/.bashrc
+    echo "alias r='more'" >> ~/.bashrc
 
     # Restart bash
-    source ../.bashrc
+    source ~/.bashrc
     
 fi
